0ee952a897cb0ba093d8f95c7ae1d5bafa2316b5
Julia2430 <martins.julia24@escola.pr.gov.br>
2026-05-03 22:02:42 -0300

    Update banco-farmacia.sql

--- banco-farmacia.sql
+++ banco-farmacia.sql
@@ -1,1 +1,25 @@
+CREATE TABLE FARMACIA 
+( 
+    CNPJ_farmacia INT PRIMARY KEY,  
+    tel_farmacia VARCHAR(20),  
+    nome_farmacia VARCHAR(100),  
+    end_farmacia VARCHAR(200)  
+); 
+
+CREATE TABLE PRODUTO 
+( 
+    cod_produto INT PRIMARY KEY,  
+    CNPJ_farmacia INT,  
+    qtd_produto INT,  
+    valor_produto DECIMAL(10,2),  
+    FOREIGN KEY (CNPJ_farmacia) REFERENCES FARMACIA (CNPJ_farmacia)
+); 
+
+CREATE TABLE FARMACEUTICO 
+( 
+    RG_farmaceutico INT PRIMARY KEY,  
+    CNPJ_farmacia INT,  
+    nome_farmaceutico VARCHAR(100),  
+    FOREIGN KEY (CNPJ_farmacia) REFERENCES FARMACIA (CNPJ_farmacia)
+); 
 
